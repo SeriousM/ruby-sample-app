24caf45ac98b112e0ad085802abf3ac3c7968b1e
SeriousM <masta2ooo@gmail.com>
2012-11-17 17:41:54 +0000

    The correct user has to be logged in to edit his user data; Friendly Forwarding is now implemented (return to url);

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -19,11 +22,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    # @user will be created by #correct_user
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -32,4 +34,20 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  private
+
+    def signed_in_user
+      # the notice hash is just a shorthand function for flash[:notice] = ...
+      # but it does not work for :success !
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
